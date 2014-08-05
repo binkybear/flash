35a57a49dc1c352c8dd232ee73ba5a5019d69da6
Binky Bear <binkybear@local>
2014-08-04 21:53:27 -1000

    kali folder path fix
    shouldn’t skip but remove previous.

--- data/tmp_kali/extractkali.sh
+++ data/tmp_kali/extractkali.sh
@@ -1,11 +1,9 @@
 #!/sbin/sh
 #
-# extract kali
+# extract kali - remove any previous folders
 #
-if [ -d "/data/local/kali"]; then
-	rm /data/local/kalifs.tar.bz2
-	echo "Kali folder detected...skipping extraction"
-else
-	tar -jxvf /data/local/kalifs.tar.bz2 -C /data/local
-	rm /data/local/kalifs.tar.bz2
+if [ -d "/data/local/kali-armhf" ]; then
+	rm -rf /data/local/kali-armhf
 fi
+tar -jxvf /data/local/kalifs.tar.bz2 -C /data/local
+rm /data/local/kalifs.tar.bz2
